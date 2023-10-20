e393cea9bc9d4903f07d5edc99ca8cc29382c3c2
Leah <leah@Uvaro-74-C-Leah.local>
2023-10-20 04:55:37 -0400

    display all the tickers and corresponding prices

--- src/StockList.tsx
+++ src/StockList.tsx
@@ -1,49 +1,77 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 
-interface StockData {
-  name: string;
-  ticker: string;
-  price: number;
-  sector: string;
-}
 const StockList = () => {
-  const [stocks, setStocks] = useState<StockData[]>([]);
   const dowTickers = [
-    "AAPL",
-    "MSFT",
-    "GS",
-    "IBM",
-    "HD",
-    "CAT",
-    "TRV",
-    "DIS",
-    "JPM",
-    "AMZN",
-    "MCD",
-    "V",
-    "NKE",
-    "CSCO",
     "AXP",
+    "AMGN",
+    "AAPL",
     "BA",
-    "UTX",
-    "INTC",
-    "WMT",
+    "CAT",
+    "CSCO",
     "CVX",
-    "KO",
-    "PG",
+    "GS",
+    "HD",
+    "HON",
+    "IBM",
+    "INTC",
     "JNJ",
-    "MRK",
-    "VZ",
-    "XOM",
-    "UNH",
+    "KO",
+    "JPM",
+    "MCD",
     "MMM",
-    "PFE",
+    "MRK",
+    "MSFT",
+    "NKE",
+    "PG",
+    "TRV",
+    "UNH",
+    "CRM",
+    "VZ",
+    "V",
+    "WBA",
+    "WMT",
+    "DIS",
+    "DOW",
   ];
+
+  const key = "ckp2nk9r01qlsp909ojgckp2nk9r01qlsp909ok0";
+  const basePath = "https://finnhub.io/api/v1";
+
+  const [stocks, setStocks] = useState<{ ticker: string; value: number }[]>([]);
+
+  const fetchData = async () => {
+    try {
+      const promises = dowTickers.map(async (ticker) => {
+        const response = await fetch(
+          `${basePath}/quote?symbol=${ticker}&token=${key}`
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        return { ticker, value: data.c };
+      });
+      const results = await Promise.all(promises);
+      setStocks(results);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div>
       <h1>Stock Data</h1>
+      <ul>
+        {stocks.map((result, index) => (
+          <li key={index}>
+            Ticker: {result.ticker}, Value: {result.value}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
