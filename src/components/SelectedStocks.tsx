094eb3d52b5131ea6ed6da28f7493776ee78cf7c
Leah <leah@uvaro-74-c-leah.home>
2023-10-23 01:37:42 -0400

    formatting

--- src/components/SelectedStocks.tsx
+++ src/components/SelectedStocks.tsx
@@ -12,23 +12,25 @@ import {
 
 const SelectedStocks = () => {
   return (
-    <Card style={{ margin: 16, borderRadius: 16, background: "#2B394E" }}>
-      <CardHeader
-        title={
-          <Typography variant="h5" fontWeight="bold" color="white">
-            Selected Stocks
-          </Typography>
-        }
-      />
-      <Box style={{ margin: 16 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={6} md={4} lg={3}></Grid>
+    <div style={{ width: "50%" }}>
+      <Card style={{ margin: 16, borderRadius: 16, background: "#2B394E" }}>
+        <CardHeader
+          title={
+            <Typography variant="h5" fontWeight="bold" color="white">
+              Selected Stocks
+            </Typography>
+          }
+        />
+        <Box style={{ margin: 16 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6} md={4} lg={3}></Grid>
+          </Grid>
           <Typography variant="body1" align="center" color="white">
             Drag and drop stocks to select stocks to add to your portfolio
           </Typography>
-        </Grid>
-      </Box>
-    </Card>
+        </Box>
+      </Card>
+    </div>
   );
 };
 
